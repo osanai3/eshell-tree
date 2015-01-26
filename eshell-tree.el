52c28574ab2fc9f9e60792d03bbb164b92a1e2c5
Koichi Osanai <osanai3@gmail.com>
2015-01-27 02:27:24 +0900

    add intermediate struct

--- eshell-tree.el
+++ eshell-tree.el
@@ -22,7 +22,8 @@
 ;; (fset 'eshell/tree (symbol-function 'eshell-tree))
 
 ;; TODO:
-;; open/close directory by click
+;; close directory by click
+;; open directory by click
 ;; max-depth
 ;; follow symlink
 
@@ -52,9 +53,64 @@
        (lambda (button)
          (find-file (button-get button 'eshell-tree-file-name)))))))
 
+(defun eshell-tree-directory-files (dirname)
+  (cl-remove-if-not
+   (symbol-function 'eshell-tree-displayable-file)
+   (directory-files-and-attributes dirname)))
+
+(defun eshell-tree-single-file-2 (filename)
+  (let ((attr (file-attributes filename)))
+    (when attr
+      (eshell-tree-show-file-from-struct
+       (eshell-tree-build-struct (cons filename attr)) "" ""))))
+
+(defun eshell-tree-make-struct (file open children)
+  (list (cons 'file file) (cons 'open open) (cons 'children children)))
+
+(defun eshell-tree-build-struct (file)
+  (cond
+   ((stringp (cadr file)) (eshell-tree-make-struct file nil nil)) ;; symlink
+   ((cadr file)
+    (eshell-tree-make-struct
+     file t
+     (let ((children (eshell-tree-directory-files (car file)))
+           (default-directory (concat default-directory (car file) "/")))
+       (mapcar 'eshell-tree-build-struct children)))) ;; directory
+   (t (eshell-tree-make-struct file nil nil)) ;; regular file
+   ))
+
+(defun eshell-tree-show-file-from-struct (struct prefix-self prefix-child)
+  (let ((file (cdr (assoc 'file struct)))
+        (open (cdr (assoc 'open struct)))
+        (children (cdr (assoc 'children struct))))
+    (concat
+     prefix-self (eshell-tree-file-button (car file))
+     (when (cadr file) (concat " " "[-]"))
+     "\n"
+     (cond
+      ((stringp (cadr file)) "") ;;symlink
+      ((cadr file)
+       (let ((default-directory (car file))
+             (preceding (reverse (cdr (reverse children))))
+             (last (car (reverse children))))
+         (concat
+          (apply 'concat
+                 (mapcar (lambda (file)
+                           (eshell-tree-show-file-from-struct
+                            file
+                            (concat prefix-child "|-- ")
+                            (concat prefix-child "|   ")))
+                         preceding))
+          (eshell-tree-show-file-from-struct
+           last (concat prefix-child "`-- ") (concat prefix-child "    "))))) ;; directory
+      (t "") ;; regular file
+      ))))
+
 (defun eshell-tree-show-file (file prefix-self prefix-child)
   (concat
-   prefix-self (eshell-tree-file-button (car file)) "\n"
+   prefix-self (eshell-tree-file-button (car file))
+   (when (cadr file) (concat " " "[-]"))
+   "\n"
    (cond
     ((stringp (cadr file)) "") ;; symlink
     ((cadr file) (eshell-tree-show-directory-content
